d9cb34b8c304f85d6f33c1a0992de3a6f59ce2ea
RWallan <3am.richardwallan@gmail.com>
2024-05-19 21:13:51 -0300

    fix: PydanticUserError - TypedDict
    Utilizando from typing_extesions import TypedDict para versões do python
    < 3.12

--- openiziai/schemas.py
+++ openiziai/schemas.py
@@ -1,4 +1,10 @@
-from typing import Any, TypedDict
+import sys
+from typing import Any
+
+if sys.version_info < (3, 12):
+    from typing_extensions import TypedDict
+else:
+    from typing import TypedDict
 
 
 class DataDict(TypedDict):
