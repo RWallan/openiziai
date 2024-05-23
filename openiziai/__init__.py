886b92cc9b51e51bb80df114db9fcdeb0af2de5e
RWallan <3am.richardwallan@gmail.com>
2024-05-23 10:31:35 -0300

    chore: bump version -> 0.1.1

--- openiziai/__init__.py
+++ openiziai/__init__.py
@@ -1,6 +1,6 @@
 """OpeniziAI, uma interface clara de como construir agentes de LLM."""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from . import agents as agents
 from . import tools as tools
